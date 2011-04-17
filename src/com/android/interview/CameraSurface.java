d936378ebedfcb6d8f48033b5d894fe7b2413cb1
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2011-04-16 21:51:01 -0400

    Updated CameraSurface activity to follow code standards.

--- src/com/android/interview/CameraSurface.java
+++ src/com/android/interview/CameraSurface.java
@@ -2,6 +2,7 @@ package com.android.interview;
 
 
 import java.io.File;
+import java.net.URI;
 
 import android.app.Activity;
 import android.content.Context;
@@ -11,82 +12,75 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Video.Media;
 import android.view.Display;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 
 public class CameraSurface extends Activity {	
-	private ImageView img;
-	private Intent cameraIntent;
-	private int CAMERA_RESULT = 0;
+	private ImageView img;	
+	private int CAMERA_RESULT = 1;
 	private int displayWidth;
 	private int displayHeight;
 	private String imageFilePath;
-	
+
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera);
+        
+        imageFilePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/image.jpg";
     }
 	
 	
-	public void takePhoto(View view)
-	{
-	       imageFilePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/image.jpg";
-	       
-	       showToast(this,imageFilePath);
-	       	               
-	        File imageFile = new File(this.imageFilePath);
-	        Uri imageFileUri = Uri.fromFile(imageFile);
-	        
-	        Intent intent = new Intent(android.provider.MediaStore.INTENT_ACTION_VIDEO_CAMERA);
-	        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageFileUri);
-	        startActivityForResult(intent, this.CAMERA_RESULT);
-		
+	public void takePhoto(View view) {		    	    
+        showToast(this,imageFilePath);       	               
+                
+        Uri imageFileUri = Uri.fromFile(new File(this.imageFilePath));
+        
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageFileUri);
+        
+        startActivityForResult(intent, this.CAMERA_RESULT);               		
 	}
 
-	public void recordVideo(View view)
-	{
-		
-	}
 	
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK){
+    protected void onActivityResult(int requestCode, int resultCode, Intent image) {
+        super.onActivityResult(requestCode, resultCode, image);
+                
+        if(requestCode == this.CAMERA_RESULT){
         	showImage();	
         }
-        
+               
     }
-    
-    private void showImage()
-    {
-    	
-		Display currentDisplay = getWindowManager().getDefaultDisplay();
+   
+
+    private void showImage() {    	
+		Display currentDisplay = getWindowManager().getDefaultDisplay();		
+		
 		this.displayWidth = currentDisplay.getWidth();
-		this.displayHeight = currentDisplay.getHeight();
-			
+		this.displayHeight = currentDisplay.getHeight();			
         this.img =  (ImageView)findViewById(R.id.image);  
         
         
     	BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
     	bmpFactoryOptions.inJustDecodeBounds = true;
-    	
-    
+    	    
 		Bitmap bmp = BitmapFactory.decodeFile(imageFilePath, bmpFactoryOptions);
     	
     	int heightRatio = (int)Math.ceil(bmpFactoryOptions.outHeight/(float)displayHeight);
     	int widthRatio = (int)Math.ceil(bmpFactoryOptions.outWidth/(float)displayWidth);
     	
-    	if(heightRatio > 1 && widthRatio > 1)
-    	{
-    		if(heightRatio > widthRatio)
-    		{
+    	if(heightRatio > 1 && widthRatio > 1) {
+    		if(heightRatio > widthRatio) {
     			bmpFactoryOptions.inSampleSize = heightRatio;
-    		} else
-    		{
+    		} else {
     			bmpFactoryOptions.inSampleSize = widthRatio;
     		}
     	}
