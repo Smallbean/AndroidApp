0535135c2ac533b89772fb5489781769c62afca9
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2011-04-30 14:26:37 -0400

    Adding resources for notes.

---
+++ src/com/android/interview/Note.java
@@ -0,0 +1,22 @@
+package com.android.interview;
+
+import com.android.interview.CameraSurface.ImageAdapter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class Note extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notes);
+
+	}
+      
+	
+}
