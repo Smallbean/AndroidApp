a3c08685ee38677b06c8e5d085f7e07eea1fe2da
Noah Cornwell <noah0078@gmail.com>
2011-04-29 23:28:17 -0400

    Updating format on Data class

--- src/com/android/interview/utilities/Data.java
+++ src/com/android/interview/utilities/Data.java
@@ -4,28 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Data {
-	   private static Data instance = null;
-	   protected Data() {
-	      // Exists only to defeat instantiation.
-	   }
-	   public static Data getInstance() {
-	      if(instance == null) {
-	         instance = new Data();
-	      }
-	      return instance;
-	   }
+	
+   private static Data instance = null;
+   
+   protected Data() {
+      // Exists only to defeat instantiation.
+   }
+   
+   public static Data getInstance() {
+      if(instance == null) {
+         instance = new Data();
+      }
+      return instance;
+   }
+   
+   public List<String> GetSubjects(){
+	   List<String> test = new ArrayList<String>();
 	   
-	   public List<String> GetSubjects(){
-		   List<String> test = new ArrayList<String>();
-		   
-		   test.add("Noah");
-		   test.add("Sean");
-		   
-		   return test;
-	   }
+	   test.add("Noah");
+	   test.add("Sean");
 	   
-	   public void AddSubject(String subject){
-		   // add the subject
-	   }
-	   
-	}
+	   return test;
+   }
+   
+   public void AddSubject(String subject){
+	   // add the subject
+   }
+   
+}
